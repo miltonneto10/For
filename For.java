3dd283ee5b0cc5aa503910a6e3f309d507e7703b
Milton Salustiano <109237764+miltonneto10@users.noreply.github.com>
2023-08-06 12:21:24 -0300

    Commit Inicial

---
+++ For.java
@@ -0,0 +1,12 @@
+//FLUXO DE REPETIÇÃO
+//Utilização do FOR
+
+class For {
+	public static void main(String args []) {
+		for (int i - 0;
+		i < 5;
+		i++) { //ordem crescente, de 0 à 5
+			System.out.println(i);
+		}
+	}
+}
